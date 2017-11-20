18540261b9ea5f438458a5d9675aee4862b96058
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-11-20 15:29:43 -0600

    validates space when player tries placing marker

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,6 @@
 public class TicTacToe {
   private String[][] board;
+  private int lastPlayer = 2; // 1 = x; 2 = o;
 
   public TicTacToe() {
     this.board = new String[3][3];
@@ -7,5 +8,27 @@ public class TicTacToe {
 
   public String[][] getBoard() {
     return this.board;
+  }
+
+  public String[][] placeMarker(int row, int column) {
+    String marker = (this.lastPlayer == 1) ? 'x' : 'o';
+    if(validatePlacement(row, column)) {
+      this.board[row][column] = marker;
+    }
+    return board;
+  }
+
+  private boolean validatePlacement(int row, int column) {
+    // check if space is valid
+    if(row < 3
+    && row >= 0
+    && column < 3
+    && column >= 0) {
+      // check if space is taken
+      if(this.board[row][column] == null) {
+        return true;
+      }
+    }
+    return false;
   }
 }
