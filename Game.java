67745c70154eb5673a7bb404f0525cd03beb94c1
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-11-20 14:44:54 -0600

    basic setup for game

---
+++ Game.java
@@ -0,0 +1,7 @@
+public class Game {
+  public static void main(String args[]) {
+    TicTacToe game = new TicTacToe();
+
+    System.out.println(game.getBoard());
+  }
+}
---
+++ TicTacToe.java
@@ -0,0 +1,11 @@
+public class TicTacToe {
+  private String[][] board;
+
+  public TicTacToe() {
+    this.board = new String[3][3];
+  }
+
+  public String[][] getBoard() {
+    return this.board;
+  }
+}
